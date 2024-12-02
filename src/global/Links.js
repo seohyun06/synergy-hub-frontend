b03a922824de4e157e790db14640ebe6dfabdb0d
[CLOUD4] 심우민 <worejj@naver.com>
2024-12-02 16:14:40 +0900

    feat: 링크 상수 설정

--- src/global/Links.js
+++ src/global/Links.js
@@ -7,8 +7,13 @@ export const ROUTES = {
     SIGNUP: {
         link: `${process.env.REACT_APP_API_URL}/members/signup`,
     },
+
     GOOGLEURL: {
         link: `${process.env.REACT_APP_API_URL}/oauth2/authorization/google`,
     },
+
+    JWTHEADER: {
+        link: `${process.env.REACT_APP_API_URL}/oauth2/jwt-header`,
+    }
     
 };
