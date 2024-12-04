5ef668d2f641405a10482162f5388053cb4716fd
[CLOUD4] 심우민 <worejj@naver.com>
2024-12-04 16:27:47 +0900

    feat: 상단 바 프로필 구현 - #22

--- src/global/AuthContext.js
+++ src/global/AuthContext.js
@@ -4,7 +4,7 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [user, setUser] = useState({ nickname: "", profileImageUrl: "" });
+    const [user, setUser] = useState({ email:"", nickname: "", profileImageUrl: "" });
 
     return (
         <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, user, setUser }}>
