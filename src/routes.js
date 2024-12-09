0894e8fe10212f992ca076c29363b54449c90f9b
[CLOUD4] 이하현 <lovehh329@naver.com>
2024-12-09 12:31:04 +0900

    remove: chat.js import 제거

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,6 @@ import LoginPage from "./page/member/loginPage";
 import SignUpPage from "./page/member/signUpPage";
 import Header from './global/Header/Header';
 import Sidebar from './global/Sidebar/Sidebar';
-import Chat from "./page/chat/Chat";
 import ChatRoom from "./page/chat/ChatRoom";
 import MyCalendar from "./global/myCalendar/MyCalendar";
 import TeamCalendar from "./page/calendar/TeamCalendar";
@@ -16,7 +15,6 @@ import NoticePage from "./page/notice/NoticePage";
 import NoticeDetailsPage from "./page/notice/NoticeDetailsPage";
 import EditNoticePage from "./page/notice/EditNoticePage";
 import OAuth2Redirect from "./page/member/Oauth2Redirect";
-import TeamChatPage from "./page/chat/TeamChatPage";
 
 // 라우트 상수 정의
 export const ROUTES = {
@@ -26,7 +24,6 @@ export const ROUTES = {
     TEAM_MAIN: '/team/main',
     TEAM_VIEW: '/team/view',
     CALENDAR: '/calendar',
-    CHAT: '/chat',
     CHAT_ROOM: '/chat/room',
     NOTICES: '/notices',
     NOTICE_CREATE: '/notice',
@@ -62,7 +59,6 @@ const router = createBrowserRouter([
     { path: `${ROUTES.TEAM_MAIN}?member=:memberId`, element: <Layout><MainPage /></Layout> },
     { path: `${ROUTES.TEAM_VIEW}?team=:teamId`, element: <Layout><TeamPage /></Layout> },
     { path: `${ROUTES.CALENDAR}?team=:teamId`, element: <Layout><TeamCalendar /></Layout> },
-    { path: ROUTES.CHAT, element: <Layout><Chat /></Layout> },
     { path: `${ROUTES.CHAT_ROOM}?team=:teamId&chatRoom=:chatRoomId`, element: <Layout><ChatRoom /></Layout> },
     { path: `${ROUTES.NOTICES}?team=:teamId`, element: <Layout><NoticePage /></Layout> },
     { path: `${ROUTES.NOTICE_CREATE}?team=:teamId`, element: <Layout><CreateNoticePage /></Layout> },
