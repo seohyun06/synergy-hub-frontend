a4ee7fa83f89b8f009b2f61b80888bd9945e054b
[CLOUD4] 이하현 <lovehh329@naver.com>
2024-12-11 18:15:54 +0900

    fix: ProtectedTeamRoute.js - 팀 접근 권한 검증

--- src/global/ProtectedTeamRoute.js
+++ src/global/ProtectedTeamRoute.js
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import {useAuth} from "./AuthContext";
 
 const ProtectedTeamRoute = ({ children }) => {
     const { teamId } = useParams();
-    console.log("teamId:", teamId);
+    const { user } = useAuth(); // 로그인된 사용자 정보 가져오기
     const [userTeams, setUserTeams] = useState([]);
-    console.log("userTeams:", userTeams);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchUserTeams = async () => {
             try {
-                const response = await axios.get('/member-teams/member/1'); // 로그인된 사용자 ID를 설정하세요
+                const response = await axios.get(`/member-teams/member/${user.id}`); // 동적으로 사용자 ID 설정
                 setUserTeams(response.data.map(team => team.id));
             } catch (err) {
                 setError('팀 정보를 가져오는 데 실패했습니다.');
@@ -22,7 +22,7 @@ const ProtectedTeamRoute = ({ children }) => {
             }
         };
         fetchUserTeams();
-    }, []);
+    }, [user.id]);
 
     if (loading) return <div>로딩 중...</div>;
     if (error) return <div>{error}</div>;
